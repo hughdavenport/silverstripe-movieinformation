85d0ff615d2f1c38b24890fc691200d73e608259
Hugh Davenport <hugh@davenport.net.nz>
2015-07-25 10:52:00 +1200

    Add MovieInformation page (admin side only)
    Adds the admin part of the movie information page. Allows CMS user to
    search for a movie and save a page. Uses the custom MovieTitleField and
    the OMDb API to test validity on save (can't publish unless it is
    valid).

--- code/model/MovieInformation.php
+++ code/model/MovieInformation.php
@@ -1,6 +1,95 @@
 <?php
 
 /**
+ * Page to display movie information
+ * Page that displays movie information retrieved from the OMDb API
+ *
+ * @package silverstripe
+ * @subpackage movieinformation
+ */
+class MovieInformation extends Page {
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getCMSFields() {
+		$self =& $this;
+		$this->beforeUpdateCMSFields(function ($fields) use ($self) {
+			$titleField = MovieTitleField::create('Title',
+			                                      _t('MovieInformation.Title'),
+			                                      $this->Title);
+			$fields->insertBefore($titleField, 'URLSegment');
+			// Create read only URL segment. Needed for the javascript to call back to the controller
+			$urlSegmentField = ReadonlyField::create('URLSegment_RO',
+			                                         _t('SiteTree.URLSegment'),
+			                                         $this->PreviewLink());
+			$fields->insertAfter($urlSegmentField, 'Title');
+			// Hide the fields we don't want
+			$fields->push(HiddenField::create('URLSegment'));
+			$fields->push(HiddenField::create('MenuTitle'));
+			$fields->push(HiddenField::create('Content'));
+		});
+		$fields = parent::getCMSFields();
+		return $fields;
+	}
+
+	protected function onBeforeWrite() {
+		parent::onBeforeWrite();
+		$this->MenuTItle = $this->Title;
+		$this->URLSegment = $this->generateURLSegment($this->Title);
+		$count = 2;
+		while(!$this->validURLSegment()) {
+			$this->URLSegment = preg_replace('/-[0-9]+$/', null, $this->URLSegment) . '-' . $count;
+			$count++;
+		}
+		$this->syncLinkTracking();
+	}
+
+	public function canPublish($member = null) {
+		return $this->isValid() !== false;
+	}
+
+	/**
+	 * Test whether this movie is valid.
+	 * Connects to OMDb API to see whether a movie with a title the same as this page exists.
+	 *
+	 * @return boolean
+	 */
+	public function isValid() {
+		$api = new RestfulService(
+			'http://www.omdbapi.com/'
+		);
+
+		$api->setQueryString(array(
+			'r'        => 'xml',        // RestfulService only supports XML
+			'type'     => 'movie',      // Only grab movies
+			't'        => $this->Title, // User supplied search
+			'plot'     => 'full',       // Get short in second query
+			'tomatoes' => 'true',       // Get rotten tomatoes ratings
+			'v'        => 1,            // Should put in for futureproof
+		));
+
+		$results = $api->request();
+		$body = $results->getBody();
+		if(!strstr($body, "<")) {
+			// An error with the xml
+			return false;
+		}
+		if($api->getAttribute($body, '' , 'root', 'response') === 'False') {
+			// server said no!
+			return false;
+		}
+		if($api->getAttributes($body, 'movie')->count() == 0) {
+			// server SHOULD have said no
+			return false;
+		}
+
+		// We should be good, return the body to save a query ;)
+		return $body;
+	}
+}
+
+/**
  * Controller for the movie information page.
  * Uses the OMDb API to search for movies, and to get information about a saved page.
  *
